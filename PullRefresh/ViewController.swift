4aeb5760f8e350b65e621b4cac46c037dffe0f18
Will <wwloading@163.com>
2015-07-30 16:40:12 +0800

    Initial Commit

---
+++ PullRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PullRefresh
+//
+//  Created by Vivien on 15/7/30.
+//  Copyright (c) 2015年 wangwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
